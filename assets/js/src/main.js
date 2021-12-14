286c6a014b500a3486e0e8fcb7dac0c4ee0ff062
wdzajicek <tig.welder4@gmail.com>
2021-12-13 21:02:04 -0600

    First commit.

---
+++ assets/js/src/main.js
@@ -0,0 +1,10 @@
+import '../../scss/bootstrap.scss';
+import '../../scss/main.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  import('bootstrap/js/src/dropdown').then(() => {
+    console.log(`Loaded Collapse`);
+    
+  })
+  
+})
