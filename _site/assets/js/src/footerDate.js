aa57225d0594d91fb4647472603a6f4caecf9bda
Wesley <tig.welder4@gmail.com>
2022-01-09 20:49:04 -0600

    Delete _site directory

--- _site/assets/js/src/footerDate.js
+++
@@ -1,9 +0,0 @@
-const COPYRIGHT_YEAR = document.getElementById('copyrightYear');
-
-function footerDate() {
-  const d = new Date;
-
-  COPYRIGHT_YEAR.innerHTML = d.getFullYear();
-}
-
-export default footerDate;
