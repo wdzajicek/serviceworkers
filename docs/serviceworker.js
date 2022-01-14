d51e854afa0d9da6b14f4ab1e1231a276ba6061a
wdzajicek <tig.welder4@gmail.com>
2022-01-13 21:13:03 -0600

    Add webmanifest, favicon, etc. Update cache version.

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -1,3 +1,5 @@
+const version = 'v4'
+
 // Install event creates a cache and populates it
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -11,25 +13,25 @@ self.addEventListener('install', (event) => {
     .then((obj) => {
       const hash = obj.hash;
 
-      caches.open('v3').then((cache) => {
+      caches.open(version).then((cache) => {
         return cache.addAll([
-          './index.html',
-          './404.html',
-          './assets/img/header-bg.jpg',
-          './favicon.ico',
-          `./assets/js/dist/main.${hash}.js`,
-          `./assets/js/dist/main.${hash}.css`,
-          `./assets/js/dist/25.${hash}.js`,
-          `./assets/js/dist/29.${hash}.js`,
-          `./assets/js/dist/58.${hash}.js`,
-          `./assets/js/dist/189.${hash}.js`,
-          `./assets/js/dist/314.${hash}.js`,
-          `./assets/js/dist/579.${hash}.js`,
-          `./assets/js/dist/636.${hash}.js`,
-          `./assets/js/dist/671.${hash}.js`,
-          `./assets/js/dist/676.${hash}.js`,
-          `./assets/js/dist/839.${hash}.js`,
-          `./assets/js/dist/909.${hash}.js`,
+          '/serviceworkers/index.html',
+          '/serviceworkers/404.html',
+          '/serviceworkers/assets/img/header-bg.jpg',
+          '/serviceworkers/favicon.ico',
+          `/serviceworkers/assets/js/dist/main.${hash}.js`,
+          `/serviceworkers/assets/js/dist/main.${hash}.css`,
+          `/serviceworkers/assets/js/dist/25.${hash}.js`,
+          `/serviceworkers/assets/js/dist/29.${hash}.js`,
+          `/serviceworkers/assets/js/dist/58.${hash}.js`,
+          `/serviceworkers/assets/js/dist/189.${hash}.js`,
+          `/serviceworkers/assets/js/dist/314.${hash}.js`,
+          `/serviceworkers/assets/js/dist/579.${hash}.js`,
+          `/serviceworkers/assets/js/dist/636.${hash}.js`,
+          `/serviceworkers/assets/js/dist/671.${hash}.js`,
+          `/serviceworkers/assets/js/dist/676.${hash}.js`,
+          `/serviceworkers/assets/js/dist/839.${hash}.js`,
+          `/serviceworkers/assets/js/dist/909.${hash}.js`,
         ]);
       });
     })
@@ -47,7 +49,7 @@ self.addEventListener('fetch', (event) => {
       return resp || fetch(event.request).then((response) => {
         let responseClone = response.clone();
 
-        caches.open('v2').then((cache) => {
+        caches.open(version).then((cache) => {
           cache.put(event.request, responseClone);
           return response;
         }).catch(err => console.error('Not found in cache and no network', err))
@@ -58,7 +60,7 @@ self.addEventListener('fetch', (event) => {
 
 // Activate event is used to delete old caches once a new service worker is activated:
 self.addEventListener('activate', (event) => {
-  const cacheKeeplist = ['v3']; // Array of cache versions to keep
+  const cacheKeeplist = [version]; // Array of cache versions to keep
 
   event.waitUntil(
     caches.keys().then((keyList) => {
